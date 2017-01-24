dcf3a30252489a81f63dc83930c49f2ec853333a
Your Name <you@example.com>
2017-01-24 22:59:20 +0200

    fix #7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,27 +5,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import {Ng2BootstrapModule} from "ng2-bootstrap";
 import {AppRoutingModule} from "./app-routing.module";
-<<<<<<< HEAD
-import {RouterOutlet} from "@angular/router";
-import { LeafletComponent } from './leaflet/leaflet.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { OpenlayersComponent } from './openlayers/openlayers.component';
-=======
 import { NavbarComponent } from './navbar/navbar.component';
 import {CommonModule} from "@angular/common";
 import {MapLayerModule} from "./map-layer/map-layer.module";
->>>>>>> 6a261f02ba3cb165e014668f097ee4d404839aaa
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    LeafletComponent,
     NavbarComponent,
-    OpenlayersComponent
-=======
-    NavbarComponent,
->>>>>>> 6a261f02ba3cb165e014668f097ee4d404839aaa
   ],
   imports: [
     BrowserModule,
@@ -33,12 +20,8 @@ import {MapLayerModule} from "./map-layer/map-layer.module";
     FormsModule,
     HttpModule,
     Ng2BootstrapModule,
-<<<<<<< HEAD
-    AppRoutingModule
-=======
     AppRoutingModule,
     MapLayerModule
->>>>>>> 6a261f02ba3cb165e014668f097ee4d404839aaa
   ],
   bootstrap: [AppComponent]
 })
