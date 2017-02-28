843bed5ce0d03b90152267df93119e42f3697ee2
yairtawil <yairtawil@gmail.com>
2017-02-28 17:18:16 +0200

    add postMessage with url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import {Router, NavigationEnd} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private router:Router){
+    router.events.filter(e => e instanceof NavigationEnd).subscribe((e)=>{
+      parent.postMessage(window.location.href, "*");
+    })
+
+  }
 }
