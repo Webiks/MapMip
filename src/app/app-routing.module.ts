ed7bc0236eb5744320bcf2aabe6a900f4cfcbffc
Harelebel <harel3108@gmail.com>
2017-03-02 15:35:09 +0200

    use hash fixing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const appRoutes:Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(appRoutes) ,
+    RouterModule.forRoot(appRoutes, {useHash:false}) ,
     MapMipModule
   ],
   exports: [RouterModule]
