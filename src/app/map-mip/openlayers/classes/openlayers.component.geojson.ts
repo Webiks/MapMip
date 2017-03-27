06ff3a9d5272533679371a3c166a9cd302822605
Harelebel <harel3108@gmail.com>
2017-03-27 17:09:38 +0300

    geojson addition

--- src/app/map-mip/openlayers/classes/openlayers.component.geojson.ts
+++ src/app/map-mip/openlayers/classes/openlayers.component.geojson.ts
@@ -8,6 +8,51 @@ import * as _ from 'lodash';
 export class OpenlayersGeoJson{
   public queryParamsSubscriber;
   public geojsonLayers:string[];
+  public  myMultiStyle = {
+  'Point': new this.openlayers.ol.style.Style({
+    image:   new this.openlayers.ol.style.Icon({
+      src: 'http://mapmip.webiks.com/assets/Markers/marker-icon-blue.png',
+      anchor: [0.5, 1]
+    })
+  }),
+    'MultiPoint': new this.openlayers.ol.style.Style({
+      image:   new this.openlayers.ol.style.Icon({
+        src: 'http://mapmip.webiks.com/assets/Markers/marker-icon-blue.png',
+        anchor: [0.5, 1]
+      })
+    }),
+  'LineString': new this.openlayers.ol.style.Style({
+    stroke: new this.openlayers.ol.style.Stroke({
+      color: '#3388ff',
+      width: 3
+    })
+  }),
+    'MultiLineString': new this.openlayers.ol.style.Style({
+      stroke: new this.openlayers.ol.style.Stroke({
+        color: '#3388ff',
+        width: 3
+      })
+    }),
+  'Polygon': new this.openlayers.ol.style.Style({
+    fill: new this.openlayers.ol.style.Fill({
+      color: 'rgba(51,136,255,0.2)'
+    }),
+    stroke: new this.openlayers.ol.style.Stroke({
+      color: '#3388ff',
+      width: 1
+    })
+  }),
+    'MultiPolygon': new this.openlayers.ol.style.Style({
+      fill: new this.openlayers.ol.style.Fill({
+        color: 'rgba(51,136,255,0.2)'
+      }),
+      stroke: new this.openlayers.ol.style.Stroke({
+        color: '#3388ff',
+        width: 1
+      })
+    })
+};
+
   constructor(private openlayers:OpenlayersComponent){
     this.queryParamsSubscriber = openlayers.activatedRoute.queryParams.subscribe(this.queryParams.bind(this));
   }
@@ -30,23 +75,53 @@ export class OpenlayersGeoJson{
         that.geojsonLayers[index]  = new that.openlayers.ol.layer.Vector({
           source: new that.openlayers.ol.source.Vector({
             format: new that.openlayers.ol.format.GeoJSON(),
-            url: url,
-            style: new ol.style.Style({
-              image: new ol.style.Circle({
-                radius: 3,
-                fill: new ol.style.Fill({color: 'white'})
-              })
-          })
-          })
+            url: url
+          }),
+          style: that.myStyleFunction.bind(that)
+
         });
         // add each elem of the array
         that.openlayers.map.addLayer(that.geojsonLayers[index])
-
       } );
 
 
-
     }
+
+   /* this.openlayers.map.on('click', function(evt) {
+      var ft = that.openlayers.map.forEachFeatureAtPixel(evt.pixel, function(f, l){return f;});
+      if (ft) {
+
+        for(var propertyName in ft.getProperties()) {
+
+            console.log(ft.getProperties()[propertyName])
+          var popup = new ol.Overlay({
+            element: this.element,
+            positioning: 'bottom-center',
+            stopEvent: false,
+
+          });
+          this.openlayers.map.addOverlay(popup);
+
+        };
+      }
+    });*/
+
+
+
   }
 
+ myStyleFunction(feature,resolution) {
+    let that = this;
+    if(feature.getGeometry().getType() == "GeometryCollection")
+    {
+      let geoColStyle =[];
+      _.forEach(feature.getGeometry().getGeometries(),function (geometry) {
+        geoColStyle.push(that.myMultiStyle[geometry.getType()])
+
+      })
+      return geoColStyle;
+
+    }
+    else return this.myMultiStyle[feature.getGeometry().getType()];
+}
 }
