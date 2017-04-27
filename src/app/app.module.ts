53320059d2c6e32cb46ff8606308945cfac679c9
Harelebel <harel3108@gmail.com>
2017-04-27 12:34:16 +0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-import {Ng2BootstrapModule} from "ng2-bootstrap";
+import {Ng2BootstrapModule} from 'ng2-bootstrap';
 import { NavbarComponent } from './navbar/navbar.component';
-import {CommonModule} from "@angular/common";
-import {MapMipModule} from "./map-mip/map-mip.module";
-import {MaterialRootModule} from "@angular/material";
+import {CommonModule} from '@angular/common';
+import {MapMipModule} from './map-mip/map-mip.module';
+import {MaterialRootModule} from '@angular/material';
 
 
 @NgModule({
