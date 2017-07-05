2e13585f2fdb9fcf72c030be4f0573ff72f2a148
harel.lebel <harel3108@gmail.com>
2017-07-05 17:03:27 +0300

    new functions to API

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import {enableProdMode, NgModuleRef} from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
+import {current} from "codelyzer/util/syntaxKind";
 
 if (environment.production) {
   enableProdMode();
@@ -31,8 +32,29 @@ public mapMipService;
   urlOverride(){
     this.mapMipService.skipLocationChange=true;
   }
-  changePosition(lat,lon){
-    this.mapMipService.changePosition(lat,lon);
+  changePosition(lat,lon) {
+    this.mapMipService.changePosition(lat, lon);
+  }
+  setZoom(zoom:number) {
+    switch (window['current'].constructor.name) {
+      case  "OpenlayersComponent" :
+        return window['current']['map'].getView().setZoom(zoom);
+      case  "LeafletComponent" :
+        return window['current']['map'].setZoom(zoom);
+      // case  "CesiumComponent" : TODO
+      // return  window['current']['map'].setZoom(zoom);
+    }
+  }
+    flyTo(latlng:[number,number],zoom:number){
+      switch (window['current'].constructor.name){
+        case  "OpenlayersComponent" :
+          return window['current']['map'].getView().setZoom(zoom);
+        case  "LeafletComponent" :
+          return  window['current']['map'].flyTo(latlng,zoom);
+        // case  "CesiumComponent" : TODO
+        // return  window['current']['map'].setZoom(zoom);
+      }
+
   }
 
 }
